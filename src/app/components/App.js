b61f51e2c1ce5de9d66948cc54c306bc37fd52e5
Luke Masters <lsm5fm@virginia.edu>
2017-04-02 11:05:03 -0500

    Test fix
    Trying to debug the state of the repo

--- src/app/components/App.js
+++ src/app/components/App.js
@@ -3,17 +3,12 @@ import { Link, withRouter} from 'react-router-dom';
 import Q from 'q';
 import '../styles/App.css';
 import SpotifyWebApi from 'spotify-web-api-js';
-<<<<<<< HEAD
 import Timer from './timer';
 import ListItem from './ListItem';
 var client_id = '9d2b0b01004541db9ea037efe0ce76d9'; // Your client id
 var client_secret = '331cfe3690b64033b376843ce22d6893'; // Your secret
 var redirect_uri = 'http://localhost:3000/'; // Your redirect uri
 import { connect } from 'react-redux';//dispatch
-=======
-import { connect,  } from 'react-redux';
->>>>>>> origin/master
-
 var s = new SpotifyWebApi();
 s.setPromiseImplementation(Q);
 
@@ -22,7 +17,6 @@ class App extends Component {
     super(props);
 
     this.state = {
-<<<<<<< HEAD
        songs : []
     }
   }
@@ -33,35 +27,6 @@ clickEvent(value) {
     });
     console.log(this.props.albumId);
   }
-
-  search(event) {
-    this.setState({
-      value: event.target.value
-    });
- 
-    s.searchAlbums(event.target.value).then((data) => {
-       console.log(data)
-       var temp = [];
-       for (var i = 0 ; i < data.albums.items.length; i++) {
-        var id = data.albums.items[i].id; 
-        temp.push(
-           <div key={i} onClick={() => {this.clickEvent(id)}} value={i}> 
-             <hr />
-             <div className="searchResult">
-              <div className="art">
-                <img src={data.albums.items[i].images[0].url} className="albumArt"/>
-              </div>
-              <div >
-                <p className="info"> {data.albums.items[i].name} </p>
-              </div>
-             </div>
-           </div>
-        )
-=======
-      albums: []
-    }
-  }
-
   search(event) {
     s.searchAlbums(event.target.value).then((data) => {
        this.setState({
@@ -85,8 +50,6 @@ clickEvent(value) {
                     </Link>
                   </div>
             )
-            
->>>>>>> origin/master
        }
        this.setState({
           albums: temp
@@ -98,18 +61,15 @@ clickEvent(value) {
     return (
       <div className="App">
         <div>
-<<<<<<< HEAD
           <input type="text" id="artistSearch" className='searchField'
           onChange={this.search.bind(this)} />
           <Button className='button'>Search</Button>
         </div>
         <Link to={"/albumpage"}> {this.state.songs} </Link>
-=======
           <input type="text"  id="artistSearch" className='searchField'
           onChange={this.search.bind(this)} />
-        </div>
+        
         {this.state.albums}
->>>>>>> origin/master
       </div>
     );
   }
@@ -118,11 +78,7 @@ clickEvent(value) {
 
 const mapStateToProps = (state) => {
   return {
-<<<<<<< HEAD
       albumId: state.albumId
-=======
-    albumId: state.albumId,
->>>>>>> origin/master
   }
 }
 
@@ -134,15 +90,8 @@ const mapDispatchToProps = (dispatch) => {
         type: "SETALBUMID",
         payload: id,
       })
-<<<<<<< HEAD
     }
-  }
-}
-export default connect(mapStateToProps, mapDispatchToProps)(App);
-=======
-    },
   }
 }
 
 export default withRouter(connect(mapStateToProps, mapDispatchToProps)(App));
->>>>>>> origin/master
