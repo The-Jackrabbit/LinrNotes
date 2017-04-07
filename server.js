e73757ffedc1c343cce73139e3a5a45d10a5b4cf
Luke Masters <lsm5fm@virginia.edu>
2017-04-06 20:37:58 -0500

    Cleanup

--- server.js
+++ server.js
@@ -10,27 +10,7 @@ const publicPath = express.static(path.join(__dirname, 'src'))
 app.use('/dist', distPath)
 app.use('/src', publicPath)
 
-const webpack = require('webpack')
-    const webpackDevMiddleware = require('webpack-dev-middleware')
-    const webpackHotMiddleware = require('webpack-hot-middleware')
-    const config = require('./webpack.config.js')
-    const compiler = webpack(config)
-
-    app.use(webpackHotMiddleware(compiler))
-    app.use(webpackDevMiddleware(compiler, {
-        noInfo: true,
-        publicPath: config.output.publicPath
-    }))
-
-app.listen(port, err => {
-    if (err) {
-        console.warn(`Error in app.listen: ${err}`)
-        return
-    }
-
-    console.log(`Listening at http://localhost:${port}`)
-})
-
+app.listen(process.env.PORT || 8080)
 /*"start": "npm run build",
     "build": "webpack -d && xcopy \"public/index.html\" \"dist/\" /F /Y && webpack-dev-server --content-base public/ --inline",
     "build:prod": "webpack -d && xcopy \"public/index.html\" \"dist/\" /F /Y",
